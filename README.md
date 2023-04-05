9fc5397c38653397d9549919ca7ac5db8e0c7c5e
maxecharles <max.charles@sydney.edu.au>
2023-04-06 09:32:38 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# TOLIMAN
+
+The primary goal of the TOLIMAN mission is to discover Earth-sized exoplanets orbiting Alpha Centauri, the closest star system to our own. To achieve this, the mission will employ a novel telescope design that will be able to detect subtle changes in the positions of the Alpha Centauri stars. These changes are caused by the gravitational reflex motion induced by an Earth-sized companion, and this cutting-edge technology will enable scientists to identify exoplanets too small to be detected by conventional telescopes.
+
+
+## This repository
+This repository is a culmination of the tasks I will be undertaking as part of a PhD project around the TOLIMAN mission.
+
+## Contact
+If you have any questions or comments about my work, please contact me at `max.charles@sydney.edu.au`. I would be happy to hear from you!
---
+++ alphacen/AlfCen.py
@@ -0,0 +1,145 @@
+"""
+Creating class for the Alpha Centauri model.
+"""
+
+
+# importing
+from jax import numpy as np
+from jax import random as jr
+import dLux as dl
+
+
+class AlfCen:
+
+    """
+    A class to to build TOLIMAN model of Alpha Centauri A and B.
+
+    ...
+
+    Attributes
+    ----------
+    sep : float
+        binary separation in arcseconds
+
+    pa : float
+        position angle in degrees
+
+    flux : float
+        average flux of the two stars in photons per frame
+
+    contrast : float
+        flux contrast between two stars
+
+    mask_dir : string
+        Disk directory of numpy array containing mask
+
+    Methods
+    -------
+    GetPSF(wavefront_npixels=256, detector_npixels=128, sampling_rate=5,):
+        Generates point spread function
+
+    AddNoise(ideal_PSF):
+        Adds poissonian and detector noise to point spread function
+
+    LinearJitter(PSF):
+        Convolves PSF with a line to simulate linear jitter.
+
+    """
+
+    # Parameters for Alpha Cen model
+    sep = 10  # binary separation in arcseconds
+    pa = 90  # position angle in degrees
+    flux = 1e4  # average flux of the two stars
+    contrast = 3.372873  # flux contrast from V band magnitudes
+    mask_dir = 'AlfCenModel/data/test_mask.npy'
+
+    # other system parameters
+    wavels = 1e-9 * np.linspace(595, 695, 3)  # wavelengths
+
+    def __init__(self, seed: int = 0):
+        self.key = [jr.PRNGKey(seed), jr.PRNGKey(seed+1)]
+        return
+
+    # loading mask, converting phase to OPD and turning mask into a layer
+    def LoadMask(self, mask_dir):
+        mask = dl.optics.AddOPD(dl.utils.phase_to_opd(np.load(mask_dir), wavelength=self.wavels.mean()))
+        return mask
+
+    def GetPSF(self,
+               mask,
+               wavefront_npixels=256,  # wavefront layer size
+               detector_npixels=128,  # detector size
+               sampling_rate=5,  # pixels per fringe i.e. 5x Nyquist
+               ):
+        """Generating PSF of Alpha Cen through TOLIMAN."""
+
+        detector_pixel_size = dl.utils.get_pixel_scale(sampling_rate, self.wavels.max(), 0.125)
+
+        # Make optical system
+        optics = dl.utils.toliman(wavefront_npixels,
+                                  detector_npixels,
+                                  detector_pixel_size=dl.utils.radians_to_arcseconds(detector_pixel_size),
+                                  extra_layers=[mask],
+                                  angular=True)
+
+        # Resetting the pixel scale of output
+        optics = optics.set(['AngularMFT.pixel_scale_out'], [dl.utils.arcseconds_to_radians(.375)])
+
+        # Creating a model Alpha Cen source
+        source = dl.BinarySource(separation=dl.utils.arcseconds_to_radians(self.sep),
+                                 wavelengths=self.wavels,
+                                 contrast=self.contrast,
+                                 flux=self.flux,
+                                 position_angle=np.deg2rad(self.pa),
+                                 )
+
+        # Creating the instrument by combining optics and source
+        tol = dl.Instrument(optics=optics, sources=[source])
+
+        # Generating and returning the PSF
+        ideal_PSF = tol.model()
+        return ideal_PSF
+
+    def LinearJitter(self, PSF):
+        """Convolving PSF with a line to simulate linear jitter.
+
+        Parameters
+        ----------
+        PSF : numpy array
+            PSF of Alpha Cen
+
+        Returns
+        -------
+        result : numpy array
+            PSF with linear jitter
+        """
+        # TODO make this random direction
+
+        # Generate kernel
+        kernel = np.eye(10, 10)
+
+        # Convolve PSF with kernel
+        result = np.convolve(PSF, kernel, mode='same')
+
+        return result
+
+
+    def AddNoise(self, PSF):
+        """Adding poissonian and detector noise to PSF.
+
+        Parameters
+        ----------
+        PSF : numpy array
+            Ideal PSF of Alpha Cen
+
+        Returns
+        -------
+        noisy_PSF : numpy array
+            Noisy PSF of Alpha Cen
+        """
+
+        noisy_PSF = jr.poisson(self.key[0], PSF)
+        det_noise = np.round(2 * jr.normal(self.key[1], noisy_PSF.shape), decimals=0).astype(int)
+        noisy_PSF += det_noise
+
+        return noisy_PSF
